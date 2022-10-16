01c88643b8cdf1e8814a477e53117a80169b48e6
Charlie <charlieryancurry@gmail.com>
2022-10-16 19:58:03 +0000

    made function typewriter with setTimeout

---
+++ typewriter.js
@@ -0,0 +1,10 @@
+const sentence = "hello there from lighthouse labs";
+let timeout = 0
+for (const char of sentence) {
+  setTimeout(() => {
+    process.stdout.write(char)
+  }, timeout)
+  timeout += 50
+} setTimeout(() => {
+  console.log()
+}, timeout)
